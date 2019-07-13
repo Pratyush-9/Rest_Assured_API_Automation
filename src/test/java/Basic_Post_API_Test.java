9aa0acff284661c827ec4d1e47fdc7d0c3cfbb7d
Pratyush Singh Kushwah <pratyushsinghkushwah>
2019-07-13 10:09:42 +0530

    Test

--- src/test/java/Basic_Post_API_Test.java
+++ src/test/java/Basic_Post_API_Test.java
@@ -3,27 +3,31 @@ import io.restassured.RestAssured;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 
 public class Basic_Post_API_Test {
 
-    Properties pro = new Properties();
+    Properties prop = new Properties();
     @BeforeTest
     public void loadVariable() throws IOException {
-        FileInputStream path = new FileInputStream("..\\.\\files\\env.properties");
-        pro.load(path);
+        String localDir = System.getProperty("user.dir");
+        File fileRelative = new File(localDir + "\\src\\test\\files\\env.properties");
+        String absPath = fileRelative.getPath();
+        InputStream path = new FileInputStream(absPath);
+        prop.load(path);
     }
 
     @Test
     public void test() {
-        RestAssured.baseURI = pro.getProperty("BaseUrl");
+        RestAssured.baseURI = prop.getProperty("BaseUrl");
         given().body("{"+ "\"name\": \"morpheus\","+ "\"job\": \"leader\""+ "}").
-                when().post(pro.getProperty("Base1")).
+                when().post(prop.getProperty("Base1")).
                 then().assertThat().statusCode(201);
     }
 }
